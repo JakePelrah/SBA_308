b1895dd3d21b1600f0853d7aeeec85d20b3d01fd
Jake Pelrah <pelrahjsp@gmail.com>
2024-08-16 23:06:23 -0400

    tables

--- main.js
+++ main.js
@@ -1,81 +1,100 @@
-// The provided course information.
-const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
+// Program data
+const {id, name:courseName, course_id, group_weight, assignments }=AssignmentGroup
+
+// populate HTML from data
+const courseTitle = document.getElementById('course-name')
+courseTitle.innerText = `${CourseInfo.name} (${CourseInfo.id})`
+
+// populate assignment details table
+const assignmentDetailsTable = document.getElementById('assignment-group-details-table')
+const [detialsHead, detailsBody]= assignmentDetailsTable.children
+detialsHead.innerHTML = `<th>Field</th><th>Value</th>`
+detailsBody.innerHTML = Object.entries(AssignmentGroup)
+.slice(0,4)
+.map(data=> `<tr><td>${data[0]}</td><td>${data[1]}</td></tr>`)
+.join('')
+
+// populate assignment table
+const assignmentsTable = document.getElementById('assignment-group-table')
+const [assignmentHead, assignementBody]= assignmentsTable.children
+assignmentHead.innerHTML = `<th>Assignment ID</th>
+<th>Name</th>
+<th>Due Date</th>
+<th>Points Possible</th>`
+assignementBody.innerHTML = AssignmentGroup.assignments
+.map(data=> `<tr>
+  <td>${data.id}</td>
+  <td>${data.name}</td>
+  <td>${data.due_at}</td>
+  <td>${data.points_possible}</td>
+  </tr>`).join('')
+
+// populate learner table
+const learnerTable = document.getElementById('submissions-table')
+const [learnerHead, learnerBody] = learnerTable.children
+learnerHead.innerHTML = `<th>Learner ID</th>
+<th>Assignment ID</th>
+<th>Submission Date</th>
+<th>Score</th>`
+learnerBody.innerHTML = LearnerSubmissions
+.map(data=> `<tr>
+  <td>${data.learner_id}</td>
+  <td>${data.assignment_id}</td>
+  <td>${data.submission.submitted_at}</td>
+  <td>${data.submission.score}</td>
+  </tr>`).join('')
+
+
+
+function validateCourse(courseInfo, assignmentGroup){
+  if (courseInfo.id !== assignmentGroup.course_id)
+    throw ("Mismatched course ID: Assignment group does not belong to this course")
+}
+
+//////////// Tests ////////////
+// validateCourse({id:23}, {course_id:2})
+
+
+function calculateScore(score, pointsPossible){
+    pointsPossible = parseFloat(pointsPossible)
+    score = parseFloat(score)
+    if(pointsPossible !== 0)
+      return score / pointsPossible * 100
+    else
+    throw Error("Division by Zero")
+}
+// console.log(calculateScore(47, 60))
+// console.log(calculateScore(47, 0))
+
+
+function isLateSubmission(dueDate, currentDate){
+  dueDate = new Date(dueDate)
+  currentDate = new Date(currentDate)
+  return dueDate.getTime() !== currentDate.getTime()
+}
+
+// console.log(isLateSubmission("2023-01-25", "2023-01-25"))
+// console.log(isLateSubmission("2023-01-25", "2023-01-26"))
+
+function calculateWeightedAverages(submissions, assignments){
+
+    
+}
+
+
+
+
+
+
+  function getLearnerData(courseInfo, assignmentGroup, submissions){
+
+    const {assignments} = assignmentGroup
+    console.log(assignments)
+
+  }
   
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
-    {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
-    }
-  ];
-  
+
+  getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions)
 //   function getLearnerData(course, ag, submissions) {
 //     // here, we would process this data to achieve the desired result.
 //     const result = [
@@ -100,3 +119,5 @@ const CourseInfo = {
   
 //   console.log(result);
   
+
+///////////////////////// DATA /////////////////////////  
